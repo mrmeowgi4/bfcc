9ce48db87568fd089342c2313f891f8059a34f7b
Saini <ssaini456123@gmail.com>
2024-10-15 14:03:07 -0700

    Add project files.

---
+++ bfcc/bfcc.c
@@ -0,0 +1,43 @@
+﻿//
+// bfcc is a brainfuck interpreter.
+// 
+// bfcc - Copyright (C) mrmeowgi4 2024-present
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct reader
+{
+	int ptr;
+	char val;
+};
+
+char* tape;
+size_t tape_size = 1024;
+
+void init_tape()
+{
+	printf("Allocating %d tape reserved bytes\n", tape_size);
+	tape = (char)malloc(tape_size);
+
+	if (tape == NULL)
+	{
+		printf("Unable to reserve tape memory.");
+		exit(-1);
+	}
+
+	//memset(tape, 0, tape_size-1);
+
+	*(tape + tape_size) = '\0'; // Use NULL char to denote the end of the tape.
+
+	printf("Done.\n");
+
+}
+
+int main()
+{
+	init_tape();
+
+	return 0;
+}
